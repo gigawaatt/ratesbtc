2ba80a4fa1d249d741bf04548755421f7efbae95
Sergey Romanov <cesarevih97@gmail.com>
2022-06-20 23:59:07 +0300

    repo.go : refactoring Init()

--- repo/repo.go
+++ repo/repo.go
@@ -8,29 +8,15 @@ import (
 	_ "github.com/lib/pq"
 )
 
-const (
-	host     = "172.27.80.1"
-	port     = 5432
-	user     = "postgres"
-	password = "user"
-	dbname   = "postgres"
-)
-
 func Init() *sql.DB {
 
-	// cfg := mysql.Config{
-	// 	User:                 env.GetVars().GetDbUsername(),
-	// 	Passwd:               env.GetVars().GetDbPasswd(),
-	// 	Net:                  "tcp",
-	// 	Addr:                 "127.0.0.1:3306",
-	// 	DBName:               "rate",
-	// 	AllowNativePasswords: true,
-	// }
+	host := GetVars().GetDbUri()
+	user := GetVars().GetDbUsername()
+	password := GetVars().GetDbPasswd()
+	dbname := GetVars().GetDBaddr()
 
-	// psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-
-	psqlconn := fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable",
-		user, password, host, port, dbname)
+	psqlconn := fmt.Sprintf("postgres://%v:%v@%v/%v?sslmode=disable",
+		user, password, host, dbname)
 
 	db, err := sql.Open("postgres", psqlconn)
 	if err != nil {
